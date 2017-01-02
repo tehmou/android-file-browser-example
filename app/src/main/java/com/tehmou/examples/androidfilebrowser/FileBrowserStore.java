1352c8dd8673488372dc30261f87fabe0eeec150
tehmou <timo.tuominen@gmail.com>
2017-01-02 20:23:03 +0200

    Change store to not to use BehaviorSubjects

--- app/src/main/java/com/tehmou/examples/androidfilebrowser/FileBrowserStore.java
+++ app/src/main/java/com/tehmou/examples/androidfilebrowser/FileBrowserStore.java
@@ -1,34 +1,49 @@
 package com.tehmou.examples.androidfilebrowser;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
-import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
 
 public class FileBrowserStore {
-    final BehaviorSubject<File> selectedFile;
-    final BehaviorSubject<List<File>> filesList;
+    // Cached values
+    private File selectedFile;
+    private List<File> filesList;
+
+    // Subjects for updates
+    private PublishSubject<File> selectedFileSubject = PublishSubject.create();
+    private PublishSubject<List<File>> filesListSubject = PublishSubject.create();
 
     public FileBrowserStore(File root) {
-        selectedFile = BehaviorSubject.create(root);
-        filesList = BehaviorSubject.create(new ArrayList<>());
+        selectedFile = root;
     }
 
     public Observable<File> getSelectedFile() {
-        return selectedFile.asObservable();
+        if (selectedFile != null) {
+            return selectedFileSubject.asObservable()
+                    .startWith(selectedFile);
+        } else {
+            return selectedFileSubject.asObservable();
+        }
     }
 
     public void putSelectedFile(File value) {
-        selectedFile.onNext(value);
+        selectedFile = value;
+        selectedFileSubject.onNext(value);
     }
 
     public Observable<List<File>> getFilesList() {
-        return filesList.asObservable();
+        if (filesList != null) {
+            return filesListSubject.asObservable()
+                    .startWith(filesList);
+        } else {
+            return filesListSubject.asObservable();
+        }
     }
 
     public void putFilesList(List<File> value) {
-        filesList.onNext(value);
+        filesList = value;
+        filesListSubject.onNext(value);
     }
 }
