fce7988eb1435d67d7cfce331cf75ef23a8ff36e
tehmou <timo.tuominen@gmail.com>
2017-03-02 17:45:45 +0100

    Change FileBrowserModel not to use BehaviorSubject for selectedFolder

--- app/src/main/java/com/tehmou/examples/androidfilebrowser/FileBrowserModel.java
+++ app/src/main/java/com/tehmou/examples/androidfilebrowser/FileBrowserModel.java
@@ -9,29 +9,39 @@ import rx.Observable;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
 
 public class FileBrowserModel {
     private static final String TAG = FileBrowserModel.class.getSimpleName();
-    private final BehaviorSubject<File> selectedFolder = BehaviorSubject.create();
-    private final Observable<List<File>> filesListObservable;
+    private final PublishSubject<File> selectedFolderSubject = PublishSubject.create();
+    private File selectedFolder;
+    private final BehaviorSubject<List<File>> filesListSubject = BehaviorSubject.create();
 
     public FileBrowserModel(Func1<File, Observable<List<File>>> getFiles) {
-        filesListObservable = selectedFolder
+        selectedFolderSubject
                 .switchMap(file ->
                         getFiles.call(file)
-                                .subscribeOn(Schedulers.io()));
+                                .subscribeOn(Schedulers.io()))
+                .subscribe(filesListSubject::onNext);
     }
 
     public Observable<File> getSelectedFolder() {
-        return selectedFolder.asObservable();
+        if (selectedFolder == null) {
+            return selectedFolderSubject.asObservable();
+        }
+
+        // Return the stream but starting with the last value first
+        return selectedFolderSubject.asObservable()
+                .startWith(selectedFolder);
     }
 
     public void putSelectedFolder(File file) {
         Log.d(TAG, "Selected file: " + file);
-        selectedFolder.onNext(file);
+        selectedFolder = file;
+        selectedFolderSubject.onNext(file);
     }
 
     public Observable<List<File>> getFilesList() {
-        return filesListObservable;
+        return filesListSubject.asObservable();
     }
 }
